afc9cef5f8a62673a179abcfda8978fee2aa864d
Steven Richards <stevenrichards@Stevens-MacBook-Air.local>
2021-04-22 13:07:16 -0700

    about page upate

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,81 @@
 import './About.css'
-
+import heading from "../pictures/Heading.jpg"
 function About () {
+    // git checkout
     return (
+<div>
         <div className="About">
-            <img src="https://bit.ly/3gpMk04" />
+            <img src={heading} />
             <h3>About Kilpatrick Craig's List</h3>
-            <p>This is the about our app</p>
-            <h3>Development Team</h3>
-            <ul>
-                <li>Richard Brennan</li>
-                <li>Michael Mason</li>
-                <li>Elgin Posadas</li>
-                <li>Steven Richards</li>
-            </ul>
+            <p>A secure platform to buy, trade, and sell with other DOD memberes.</p>
         </div>
+
+        <h2>Our Team</h2>
+        <div className="row">
+            <div className="column">
+                <div className="card">
+                    <img src="https://bit.ly/32DBBHd" alt="Richard"/>
+                    <div className="container">
+                        <h2>Richard Brennen</h2>
+                        <p className="title">Tallest Guy</p>
+                        <p>Does cool stuff.</p>
+                        <p>Richard@kpcl.com</p>
+                        <p><button className="button" onClick={(e) => {
+                            e.preventDefault()
+                            window.open('https://www.linkedin.com/in/richie-brennan/')
+                            }}>Contact</button></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                    <img src="https://bit.ly/3equlnK" alt="Steven"/>
+                    <div className="container">
+                        <h2>Steven Richards</h2>
+                        <p className="title">Second Tallest Guy</p>
+                        <p>Does not do cool stuff</p>
+                        <p>Steven@kpcl.com</p>
+                        <p><button className="button" onClick={(e) => {
+                            e.preventDefault()
+                            window.open('https://www.linkedin.com/in/steven-lance-richards-89bb93149/')
+                            }}>Contact</button></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                    <img src="https://bit.ly/32Hkgxf" alt="Michael"/>
+                    <div className="container">
+                        <h2>Michael Mason</h2>
+                        <p className="title">Third Tallest Guy</p>
+                        <p>Does cool boom stuff</p>
+                        <p>Michael@kpcl.com</p>
+                        <p><button className="button" onClick={(e) => {
+                            e.preventDefault()
+                            window.open('https://www.linkedin.com/in/michael-mason-8825985a/')
+                            }}>Contact</button></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                    <img src="https://bit.ly/2RQFNkJ" alt="Elgin"/>
+                    <div className="container">
+                        <h2>Elgin Posadas</h2>
+                        <p className="title">Fourth Tallest Guy</p>
+                        <p>Runs his roomates show</p>
+                        <p>Elgin@kpcl.com</p>
+                        <p><button className="button">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
+
     )
 }
 
